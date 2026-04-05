5c4c9f4cfb6b39ed139a1dbcaeefd6015628ac84
Kumaraditya8527 <adityakumar1886@gmail.com>
2026-04-05 16:34:09 +0530

    fix: update logoutUser to set refreshToken to 1 instead of undefined

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -122,7 +122,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     User.findByIdAndUpdate(req.user._id,
         {
             $set:{
-                refreshToken: undefined
+                refreshToken: 1
             }
         },
         {
