a13fcbdc3fffa10ff9aac35d35838fef179350e2
Kumaraditya8527 <adityakumar1886@gmail.com>
2026-04-08 19:06:51 +0530

    chore: add comments to clarify user schema methods and pre-save hook functionality

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -9,8 +9,8 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         lowercase: true,
-        trim: true,
-        index: true
+        trim: true,//remove whitespace from both ends of the string
+        index: true //for faster search and retrieval of users by username
     },
     email:{
         type: String,
@@ -47,19 +47,19 @@ const userSchema = new mongoose.Schema({
     }
 },{timestamps: true})
 
-userSchema.pre("save", async function(next){
+userSchema.pre("save", async function(next){  //pre save hook to hash the password before saving the user document to the database
     if(!this.isModified("password")){
         return next()
     }
-    this.password=await bcrypt.hash(this.password, 10)
+    this.password=await bcrypt.hash(this.password, 10) //hash the password with a salt round of 10, which adds an extra layer of security to the hashed password
     next()
 })
 
-userSchema.methods.isPasswordCorrect = async function(password){
+userSchema.methods.isPasswordCorrect = async function(password){ //compare the provided password with the hashed password stored in the database using bcrypt's compare method, which returns true if the passwords match and false otherwise
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.generateAccessToken = function(){
+userSchema.methods.generateAccessToken = function(){//generate a JWT access token containing the user's id, email, username, and fullname as the payload, signed with a secret key and an expiration time defined in the environment variables
     return jwt.sign(
         {
             _id: this._id,
